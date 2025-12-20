d75963ad3e3498d580bc5433cdc56f643a3023e0
MD Shoriful Islam <cmshohelrana50@gmail.com>
2025-12-20 11:49:54 +0600

    Update app.js

--- 70.Captcha Project/app.js
+++ 70.Captcha Project/app.js
@@ -1,8 +1,8 @@
-let captcha = document.querySelector('.captcha');
-let reloadBtn = document.querySelector('.reload-btn');
-let inputField = document.querySelector('input');
-let checkBtn = document.querySelector('.check-btn');
-let statusTxt = document.querySelector('.status-text');
+// let captcha = document.querySelector('.captcha');
+// let reloadBtn = document.querySelector('.reload-btn');
+// let inputField = document.querySelector('input');
+// let checkBtn = document.querySelector('.check-btn');
+// let statusTxt = document.querySelector('.status-text');
 
 let allCharacters = [
   'A',
@@ -69,34 +69,34 @@ let allCharacters = [
   9,
 ];
 
-checkBtn.addEventListener('click', e => {
-  e.preventDefault();
-  statusTxt.style.display = 'block';
-  let inputVal = inputField.value.split('').join('');
-  if (inputVal == captcha.innerText) {
-    statusTxt.style.color = '#4db2ec';
-    statusTxt.innerText = 'Nice, Captcha Matched';
-    setTimeout(() => {
-      statusTxt.style.display = '';
-      inputField.value = '';
-      captcha.innerText = '';
-      getCaptcha();
-    }, 4000);
-  } else {
-    statusTxt.style.color = '#ff0000';
-    statusTxt.innerText = 'Captcha not matched. Please Try Again Later';
-  }
-});
+// checkBtn.addEventListener('click', e => {
+//   e.preventDefault();
+//   statusTxt.style.display = 'block';
+//   let inputVal = inputField.value.split('').join('');
+//   if (inputVal == captcha.innerText) {
+//     statusTxt.style.color = '#4db2ec';
+//     statusTxt.innerText = 'Nice, Captcha Matched';
+//     setTimeout(() => {
+//       statusTxt.style.display = '';
+//       inputField.value = '';
+//       captcha.innerText = '';
+//       getCaptcha();
+//     }, 4000);
+//   } else {
+//     statusTxt.style.color = '#ff0000';
+//     statusTxt.innerText = 'Captcha not matched. Please Try Again Later';
+//   }
+// });
 
-function getCaptcha() {
-  for (let i = 0; i < 6; i++) {
-    let randomChar =
-      allCharacters[Math.floor(Math.random() * allCharacters.length)];
-    captcha.innerText += `${randomChar}`;
-  }
-}
+// function getCaptcha() {
+//   for (let i = 0; i < 6; i++) {
+//     let randomChar =
+//       allCharacters[Math.floor(Math.random() * allCharacters.length)];
+//     captcha.innerText += `${randomChar}`;
+//   }
+// }
 
-reloadBtn.addEventListener('click', () => {
-  captcha.innerHTML = '';
-  getCaptcha();
-});
+// reloadBtn.addEventListener('click', () => {
+//   captcha.innerHTML = '';
+//   getCaptcha();
+// });
