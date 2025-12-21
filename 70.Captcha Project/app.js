137fdfb76d466009d3374586640141a17f24d708
MD Shoriful Islam <cmshohelrana50@gmail.com>
2025-12-21 16:04:27 +0600

    Uncomment captcha validation logic

--- 70.Captcha Project/app.js
+++ 70.Captcha Project/app.js
@@ -71,22 +71,22 @@ let allCharacters = [
 
 checkBtn.addEventListener('click', e => {
   e.preventDefault();
-//   statusTxt.style.display = 'block';
-//   let inputVal = inputField.value.split('').join('');
-//   if (inputVal == captcha.innerText) {
-//     statusTxt.style.color = '#4db2ec';
-//     statusTxt.innerText = 'Nice, Captcha Matched';
-//     setTimeout(() => {
-//       statusTxt.style.display = '';
-//       inputField.value = '';
-//       captcha.innerText = '';
-//       getCaptcha();
-//     }, 4000);
-//   } else {
-//     statusTxt.style.color = '#ff0000';
-//     statusTxt.innerText = 'Captcha not matched. Please Try Again Later';
-//   }
-// });
+  statusTxt.style.display = 'block';
+  let inputVal = inputField.value.split('').join('');
+  if (inputVal == captcha.innerText) {
+    statusTxt.style.color = '#4db2ec';
+    statusTxt.innerText = 'Nice, Captcha Matched';
+    setTimeout(() => {
+      statusTxt.style.display = '';
+      inputField.value = '';
+      captcha.innerText = '';
+      getCaptcha();
+    }, 4000);
+  } else {
+    statusTxt.style.color = '#ff0000';
+    statusTxt.innerText = 'Captcha not matched. Please Try Again Later';
+  }
+});
 
 // function getCaptcha() {
 //   for (let i = 0; i < 6; i++) {
